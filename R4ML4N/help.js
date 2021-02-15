b679bc04efb44c24b7066af32da27927368227d6
tobishit <79066500+tobishit@users.noreply.github.com>
2021-02-15 01:39:00 -0300

    Update help.js

--- R4ML4N/help.js
+++ R4ML4N/help.js
@@ -1,26 +1,28 @@
 const help = (prefix) => {
-	return `🔰---[ *MENU ARR157-BOT* ]---🔰
+	return `🔥---[ *MENU Tobi-BOT* ]---🔥
 ┏━━━━━━━━━━━━━━━━━━━━┓
 ┃ *⚠️ NAO SPAM! NAO CALL!!! ⚠️*
 ┃   *caso aocontrario  BLOCK*
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃⊱❥ PROPRIETÁRIO : *BRUXINHO*
+┃⊱❥ PROPRIETÁRIO : *TOBI*
 ┃⊱❥ ATIVO : *08:00-22:00 WIB*
 ┃⊱❥ PREFIX : 「 ${prefix} 」
 ┃⊱❥ VERSÃO : 1.1
-┃⊱❥ número do meu criador:wa.me/+27624213430
-┃⊱❥ Da uma FORÇA la no meu canal fml:https://youtube.com/channel/UCalQOjX5BFqim6x8JyCMM-w
-┃⊱❥ LINK do meu GRUPO: https://chat.whatsapp.com/FvPEpkaPrFXAErjML3ekyW
-┃⊱❥ *SE FOR KIBAR DEIXA OS CRÉDITOS MAN*
+┃⊱❥ número do meu criador:wa.me/+5531984159437
+┃⊱❥
+┃⊱❥
+┃⊱❥ Link do grupo: https://chat.whatsapp.com/KCV4iZ2TrRm1D0whPyCmrv
+┃⊱❥ SEJAM BEM VINDO(A)
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑻𝒆𝒏𝒕𝒂𝒏𝒈 𝑩𝑶𝑻🌀
+┃ 🔥𝑻𝒆𝒏𝒕𝒂𝒏𝒈 𝑩𝑶𝑻🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}info
 ┃➢ ${prefix}owner
 ┃➢ ${prefix}donasi
 ┃➢ ${prefix}blocklist
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑺𝑰𝑴𝑷𝑳𝑬S🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑺𝑰𝑴𝑷𝑳𝑬S🔥
+
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}sticker
 ┃➢ ${prefix}toimg
@@ -29,13 +31,13 @@ const help = (prefix) => {
 ┃➢ ${prefix}nulis
 ┃➢ ${prefix}quotes
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑫𝑶𝑾𝑵𝑳𝑶𝑨𝑫🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑫𝑶𝑾𝑵𝑳𝑶𝑨𝑫🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}tiktod
 ┃➢ ${prefix}ytmp3
 ┣━━━━━━━━━━━━━━━━━━━━┛
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑮𝑨𝑩𝑼𝑻𝒁🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑮𝑨𝑩𝑼𝑻𝒁🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}apakah
 ┃➢ ${prefix}kapankah
@@ -47,12 +49,12 @@ const help = (prefix) => {
 ┃➢ ${prefix}dare
 ┣━━━━━━━━━━━━━━━━━━━━┛
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑴𝑬𝑴𝑬🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑴𝑬𝑴𝑬🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}meme
 ┃➢ ${prefix}memeindo
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑮𝑹𝑶𝑼𝑷🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑮𝑹𝑶𝑼𝑷🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}admin
 ┃➢ ${prefix}welcome [1/0]
@@ -68,7 +70,7 @@ const help = (prefix) => {
 ┃➢ ${prefix}hidetag
 ┃➢ ${prefix}simih [1/0]
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑪𝑶𝑴𝑳𝑰🌀 ✖[ERRO]✖
+┃ 🔥𝑴𝒆𝒏𝒖 𝑪𝑶𝑴𝑳𝑰🔥 ✖[ERRO]✖
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}randomhentong
 ┃➢ ${prefix}randomloli
@@ -77,7 +79,7 @@ const help = (prefix) => {
 ┃➢ ${prefix}nsfwtrap
 ┣━━━━━━━━━━━━━━━━━━━━┛
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑶𝑻𝑯𝑬𝑹🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑶𝑻𝑯𝑬𝑹🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}ssweb
 ┃➢ ${prefix}simi
@@ -88,7 +90,7 @@ const help = (prefix) => {
 ┃➢ ${prefix}ytstalk
 ┣━━━━━━━━━━━━━━━━━━━━┛
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑴𝑨𝑲𝑬𝑹🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑴𝑨𝑲𝑬𝑹🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}hartatahta
 ┃➢ ${prefix}pornhub
@@ -101,18 +103,18 @@ const help = (prefix) => {
 ┃➢ ${prefix}lovetext
 ┃➢ ${prefix}gugeltext
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃ 🌀𝑴𝒆𝒏𝒖 𝑶𝑾𝑵𝑬𝑹🌀
+┃ 🔥𝑴𝒆𝒏𝒖 𝑶𝑾𝑵𝑬𝑹🔥
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃➢ ${prefix}setprefix
 ┃➢ ${prefix}bc
 ┃➢ ${prefix}clearall
 ┃➢ ${prefix}clone
 ┣━━━━━━━━━━━━━━━━━━━━┛
-┃⟪ CRIADO POR BY *BRUXINHO MODS* ⟫
+┃⟪ CRIADO POR *TOBI OF HYPE* ⟫
 ┣━━━━━━━━━━━━━━━━━━━━┓
 
              ╲    ╱    ● ᏴϴͲ●ᎷᎬΝႮ●
-       ╱▔▔▔▔▔╲       Autor    : ARR157 BOT
+       ╱▔▔▔▔▔╲       Autor    : TOBI BOT
       ┃┈▇┈┈▇┈┃      
 ╭╮┣━━━━━━┫╭╮    
 ┃┃┃┈┈┈┈┈┈┃┃┃    
